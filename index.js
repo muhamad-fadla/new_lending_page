7958670cd009ee2348166fdc03d7d52788932b90
muhamad-fadla <yuratamma@gmail.com>
2021-10-10 12:30:30 +0700

    update

--- index.js
+++ index.js
@@ -22,6 +22,24 @@ app.disable('x-powered-by')
 
 app.use(expressSitemapXml(getUrls, 'https://my-topup.store'))
 
+app.use((req,res,next) => {
+	 res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  next();
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('*', async (req,res) => {
